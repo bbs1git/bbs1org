53476fafc0dee72703b0b8d5f90c2b62c81f3375
bbs1git <326511099+bbs1git@users.noreply.github.com>
2026-07-30 20:17:37 +0800

    chore: 升级版本至 v6.16

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ ini_set('display_errors', '0');
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 date_default_timezone_set('Asia/Shanghai');
 define('APP_START_TIME', microtime(true));
-define('APP_VERSION', 'v6.15');
+define('APP_VERSION', 'v6.16');
 define('SQL_DEBUG_MODE', false);
 define('APP_ROOT', __DIR__);
 define('APP_DIR', APP_ROOT . '/app');
